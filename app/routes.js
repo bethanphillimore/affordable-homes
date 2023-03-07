b25bb7d4d8d05632d8f48d1314918a3fc7b43f94
lynsey-reynolds <lynsey.reynolds@methods.co.uk>
2023-03-07 16:26:16 +0000

    Small changes and addition of two new pages
    - Changed buttons to say 'save and continue' instead of save
    - Removed previous test text from scheme page
    - Created and commented out code in routes js to save until we can fix
    - Added building info page
    - Added conditional question from home info

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,21 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+// Run this code when a form is submitted to 'home-type-home-info'
+// router.post('/home-type-home-info', function (req, res) {
+
+    // Make a variable and give it the value from question
+//    var specificGroup = req.session.data['part-needs-yes']
+  
+    // Check whether the variable matches a condition
+//    if (specificGroup == "yes"){
+      // Send user to next page
+ //     res.redirect('/home-type-home-information-conditional')
+ //   } else {
+      // Send user to ineligible page
+ //     res.redirect('/home-type-building')
+ //   }
+  
+//  })
